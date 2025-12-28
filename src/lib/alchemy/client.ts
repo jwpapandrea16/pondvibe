90b395d3266d01ad2709523858758a1eb5929dc3
Vibecode Terminal <terminal@vibecodeapp.com>
2025-12-28 20:07:27 +0000

    Sandbox autosave

--- src/lib/alchemy/client.ts
+++
@@ -1,8 +0,0 @@
-import { Alchemy, Network } from 'alchemy-sdk'
-
-const config = {
-  apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY!,
-  network: Network.ETH_MAINNET,
-}
-
-export const alchemy = new Alchemy(config)
